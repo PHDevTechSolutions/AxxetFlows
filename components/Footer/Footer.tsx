57a04cd10099f04533e898cac40ea20c104a54e8
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-05-16 14:22:03 +0800

    Leroux Y Xchire

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-green-700 text-white font-bold py-4">
-      <div className="container mx-auto text-center text-xs">
+      <div className="container mx-auto text-right text-xs">
         <p>&copy; {new Date().getFullYear()} PH-Devtech Solutions | Inventory Management System </p>
       </div>
     </footer>
